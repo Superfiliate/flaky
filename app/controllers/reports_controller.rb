b46fe67ab66f5c1bb56a74212ccef3ccc7f95a67
Evandro Sasse <evsasse@gmail.com>
2024-12-10 16:48:58 -0300

    page to inspec all the files inside the zip

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,27 +6,36 @@ class ReportsController < ApplicationController
   protect_from_forgery except: %i[bundled_html]
 
   def bundled_html
-    return redirect_to "#{request.original_url}/index.html" if bundled_path.blank?
-
     # TODO: Need to check the authorization...
     # TODO: Rescue and render some errors
     @report = Report.find(params[:id])
 
-    # render json: { original_url: request.original_url, bundled_path:, report: }
+    if lookup_path.blank?
+      @items = with_zip_entries do |entries|
+        entries.map do |entry|
+          without_prefix = entry.name.split("/", 2)[1]
+          next if without_prefix.blank?
+
+          { href: "#{request.original_url}/#{without_prefix}", name: without_prefix }
+        end.compact
+      end
+
+      return
+    end
 
     # https://stackoverflow.com/questions/9233021/returning-files-from-rails
     # https://apidock.com/rails/ActionController/DataStreaming/send_data
     send_data(
       extract_file,
       disposition: "inline",
-      filename: bundled_path.split("/").last,
+      filename: lookup_path.split("/").last,
       # :type => "mime/type"
     )
   end
 
   private
 
-  def bundled_path
+  def lookup_path
     with_slash = request.original_url.split("bundled_html")[1] || ""
     with_slash[1..-1]
   end
@@ -36,11 +45,19 @@ class ReportsController < ApplicationController
   def extract_file
     @report.bundled_html.open do |tempfile|
       Zip::File.open(tempfile.path) do |zip_entries|
-        entry = zip_entries.glob("*/#{bundled_path}").first
+        entry = zip_entries.glob("*/#{lookup_path}").first
         return nil if entry.nil?
 
         raise "File too large when extracted" if entry.size > MAX_SIZE_RESPONSE
         entry.get_input_stream.read
+      end
+    end
+  end
+
+  def with_zip_entries
+    @report.bundled_html.open do |tempfile|
+      Zip::File.open(tempfile.path) do |zip_entries|
+        yield zip_entries
       end
     end
   end
