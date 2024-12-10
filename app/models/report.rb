c46e2da66ba939067030a255f395f0ba5a611eff
Evandro Sasse <evsasse@gmail.com>
2024-12-10 15:25:56 -0300

    simple validation on report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,12 @@
 class Report < ApplicationRecord
+  KINDS = %w[simplecov].freeze
+
+  enum :kind, KINDS.index_with(&:to_s)
+
   belongs_to :organization
   belongs_to :project
 
   has_one_attached :bundled_html
+
+  validates :name, :kind, presence: true
 end
